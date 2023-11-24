eb5bd59fae4b3e2719a6d8fc5e1e539166a87616
SkyOPG <nicknameopg1@gmail.com>
2023-11-24 13:00:00 +0000

    quick update

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const logExit = (a, b = 1) => {
     process.exit(b);
 }
 
+async function ensureAuth(req, res, next){
+    const { username, password } = req.body || req.query;
+
+    const isTaken = await usernames.get(username);
+    if(!isTaken)
+        return res.status(404).send("the user doesn't exist")
+
+    const pass = await bcrypt.compare(password, isTaken);
+    if(!pass)
+        return res.status(401).send("Wrong password");
+
+    next();
+}
+
 const config = fs.existsSync("ss-srv.yml")
     ? yaml.parse(fs.readFileSync("ss-srv.yml"))
     : logExit("No ss-srv.yml found, exiting...");
@@ -18,6 +32,11 @@ const config = fs.existsSync("ss-srv.yml")
 const usernames = new QuickDB({
     filePath: "clouds.db",
     table: "usernames"
+})
+
+const clouds = new QuickDB({
+    filePath: "clouds.db",
+    table: "clouds"
 })
 
 app.use(express.json());
@@ -34,16 +53,29 @@ app.post("/signup", async function(req, res){
     }
 });
 
-app.post("/login", async function(req, res){
-    const { username, password } = req.body;
+app.post("/login", ensureAuth ,async function(req, res){
+    return res.status(200).send("Correct!")
+});
 
-    const isTaken = await usernames.get(username);
-    if(!isTaken)
-        return res.status(404).send("the user doesn't exist")
+app.get("/clouds/get", async function(req, res){
+    const { cloud } = req.query;
+    const cloudQuery = await clouds.get(cloud);
 
-    const pass = bcrypt.compare(password, isTaken)
+    if(!cloudQuery)
+        return res.status(404).send("Not found");
+
+    return fs.existsSync("./clouds/"+cloud+".ssx")
+        ? res.status(200)
+            .send({
+                file: fs.readFileSync(`./clouds/${cloud}.ssx`),
+                name: cloud,
+                cloudQuery
+            })
+        : res.status(500)
+            .send("Error: Database seems to have issues finding the package")
 })
 
+app.post("/clouds")
 app.listen(config.port, console.log(
     "ready on ::"
         + config.port
